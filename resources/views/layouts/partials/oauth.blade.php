43de92368dbb743aff55bb2794e04a22016614bb
Richard Vannauker <richard.vannauker@gmail.com>
2017-01-31 05:31:06 +0000

    Increase legibility of blade template for social oauth login

--- resources/views/layouts/partials/oauth.blade.php
+++ resources/views/layouts/partials/oauth.blade.php
@@ -3,5 +3,12 @@
     $social = ['facebook', 'google', 'twitter', 'reddit', 'linkedin', 'pinterest', 'instagram', 'yahoo', 'openid', 'tumblr'];
 @endphp
 @foreach($social as $company)
-    {{ Html::link("social/login/redirect/${company}", Html::tag('span', '', ['class' => "fa fa-${company}"]), ['class' => "btn btn-social-icon btn-${company}"], null, false) }}
+    {{
+        Html::link("social/login/redirect/${company}",
+            Html::tag('span', '', ['class' => "fa fa-${company}"]),
+            ['class' => "btn btn-social-icon btn-${company}"],
+            null,
+            false
+        )
+    }}
 @endforeach
