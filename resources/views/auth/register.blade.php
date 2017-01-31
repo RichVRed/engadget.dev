8910d289c2e23468a5ae9fd82417922beeb4d3e6
Richard Vannauker <richard.vannauker@gmail.com>
2017-01-31 06:23:08 +0000

    Get Register functionality to work with ReCaptcha / NoCaptcha

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,55 +3,67 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+        <div class="col-md-6 col-md-offset-3">
+            <div class="panel panel-default col-md-12">
+                <div class="panel-heading">NEW ACCOUNT</div>
                 <div class="panel-body">
                     {!! Form::open(['url' => '/user/register', 'method' => 'post', 'role' => 'form', 'class' => 'form-horizontal']) !!}
                         {{ csrf_field() }}
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-12 col-md-offset-0">Email</label>
+                            <div class="col-md-12 col-md-offset-0">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                @if ($errors->has('email'))
+                                    <span class="help-block"><strong>{{ $errors->first('email') }}</strong></span>
                                 @endif
                             </div>
                         </div>
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-12 col-md-offset-0">Username</label>
+                            <div class="col-md-12 col-md-offset-0">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                @if ($errors->has('name'))
+                                    <span class="help-block"><strong>{{ $errors->first('name') }}</strong></span>
                                 @endif
                             </div>
                         </div>
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-                            <div class="col-md-6">
+                            <label for="password" class="col-md-12 col-md-offset-0">Password</label>
+                            <div class="col-md-12 col-md-offset-0">
                                 <input id="password" type="password" class="form-control" name="password" required>
                                 @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
+                                    <span class="help-block"><strong>{{ $errors->first('password') }}</strong></span>
                                 @endif
                             </div>
                         </div>
-                        <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-                            <div class="col-md-6">
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password-confirm" class="col-md-12 col-md-offset-0">Re-enter Password</label>
+                            <div class="col-md-12 col-md-offset-0">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
                             </div>
                         </div>
-                        {!! Captcha::display() !!}
+                        <div class="form-group{{ $errors->has('tos') ? ' has-error' : '' }}">
+                            <label for="tos" class="col-md-12 col-md-offset-0">
+                                {{
+                                    Form::checkbox('tos', 1, old('tos'), ['name' => 'tos', 'required'])
+                                }} I have read and agree to the {{
+                                    Html::link('http://legal.aol.com/TOS/', 'TOS', [], null, false)
+                                }} and {{
+                                    Html::link('https://privacy.aol.com/', 'Privacy Policy', [], null, false)
+                                }}
+                            </label>
+                        </div>
+                        <div class="form-group{{ $errors->has('g-recaptcha-response') ? ' has-error' : '' }}">
+                            <div class="col-md-12 col-md-offset-0">
+                                @if ($errors->has('g-recaptcha-response'))
+                                    <span class="help-block"><strong>{{ $errors->first('g-recaptcha-response') }}</strong></span>
+                                @endif
+                                {!! Captcha::display() !!}
+                            </div>
+                        </div>
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">Register</button>
+                            <div class="col-md-12">
+                                <button type="submit" class="btn btn-primary">Register</button> or @include('layouts.partials.oauth')
                             </div>
                         </div>
                     {!! Form::close() !!}
@@ -60,4 +72,5 @@
         </div>
     </div>
 </div>
+{!! Captcha::script() !!}
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,83 +2,82 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1"/>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="cache-control" content="no-store" />
+    <meta http-equiv="expires" content="0" />
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta http-equiv="content-type" content="text/html" />
 
+    <title>{{ config('app.name', env('APP_NAME')) }} | Technology News, Advice and Features</title>
+    <meta name="description" content="Engadget is a web magazine with obsessive daily coverage of everything new in gadgets and consumer electronics">
+    <link rel="canonical" href="{{ config('app.url', env('APP_URL')) }}">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.engadget.com/">
+    <meta property="og:title" content="Engadget | Technology News, Advice and Features">
+    <meta property="og:image" content="http://localhost/images/eng-logo-928x201.png">
+    <meta property="og:description" content="Engadget is a web magazine with obsessive daily coverage of everything new in gadgets and consumer electronics">
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@engadget">
+    <meta name="twitter:url" content="https://www.engadget.com/">
+    <meta name="twitter:title" content="Engadget | Technology News, Advice and Features">
+    <meta name="twitter:description" content="Engadget is a web magazine with obsessive daily coverage of everything new in gadgets and consumer electronics">
+    <meta name="twitter:image" content="http://localhost/images/eng-logo-928x201.png">
+
+    <meta name="activeEdition" content="usa">
+    <!-- Google Site Verification -->
+    <meta name="google-site-verification" content="Engadget" />
+    <!-- Bitly Site Verification -->
+    <meta name="bitly-verification" content="Engadget" />
+    <!-- Pintrest Site Verification -->
+    <meta name="p:domain_verify" content="Engadget" />
+    <!-- Microsoft Site Validation -->
+    <meta name="msvalidate.01" content="Engadget" />
+    <!-- ViewPort Sizes -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- iOS -->
+    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+    <meta name="apple-touch-fullscreen" content="yes"/>
+    <!-- Facebook -->
+    <meta property="fb:admins" content="Engadget" />
+    <meta property="fb:app_id" content="Engadget" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:site_name" content="Engadget" />
+    {{ Html::meta('msapplication-TileImage', url('images/eng-e-558.png')) }}
+    <!-- Windows 8 Tiles -->
+    <meta name="application-name" content="Engadget">
+    <meta name="msapplication-TileImage" content="{{ url('/assets/images/apple-touch-icon-144x144.png') }}'">
+    <meta name="msapplication-TileColor" content="#2B2D32">
+    <!-- IE 11 Tile -->
+    <meta name="msapplication-tooltip" content="Engadget">
+    <meta name="msapplication-config" content="{{ url('/assets/xml/ieconfig.xml') }}">
+    {{ Html::meta('msapplication-TileColor', '#2B2D32') }}
+    {{ Html::meta('msapplication-TileImage', url('images/favicon/mstile-144x144.png')) }}
+    {{ Html::meta('msapplication-square70x70logo', url('images/favicon/mstile-70x70.png')) }}
+    {{ Html::meta('msapplication-square150x150logo', url('images/favicon/mstile-150x150.png')) }}
+    {{ Html::meta('msapplication-wide310x150logo', url('images/favicon/mstile-310x150.png')) }}
+    {{ Html::meta('msapplication-square310x310logo', url('images/favicon/mstile-310x310.png')) }}
+    {{ Html::meta('msapplication-notification', 'frequency=30;polling-uri=http://notifications.buildmypinnedsite.com/?feed=engadget.dev/rss.xml&amp;id=1;polling-uri2=http://notifications.buildmypinnedsite.com/?feed=engadget.dev/rss.xml&amp;id=2;polling-uri3=http://notifications.buildmypinnedsite.com/?feed=engadget.dev/rss.xml&amp;id=3;polling-uri4=http://notifications.buildmypinnedsite.com/?feed=engadget.dev/rss.xml&amp;id=4;polling-uri5=http://notifications.buildmypinnedsite.com/?feed=engadget.dev/rss.xml&amp;id=5;cycle=1') }}
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
+    <!-- RSS -->
+    <link rel="alternate" type="application/rss+xml" title="Engadget" href="{{ url('/rss.xml') }}"/>
+    @include('layouts.partials.favicons')
     <!-- Styles -->
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
-
     <!-- Scripts -->
     <script>
         window.Laravel = {!! json_encode([ 'csrfToken' => csrf_token(), ]) !!};
     </script>
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                            <li><a href="{{ url('/register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ url('/logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        @yield('content')
-    </div>
-    <!-- Scripts -->
-    <script src="{{ mix('/js/app.js') }}"></script>
+<div id="app">
+    @include('layouts.partials.navigation')
+    @yield('content')
+</div>
+<!-- Scripts -->
+<script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
